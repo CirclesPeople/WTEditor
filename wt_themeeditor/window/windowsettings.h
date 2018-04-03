ece64bdc6302733f5baa3170e33153de6b3fd1f7
huangyabin <huangyabin@wingtech.com>
2018-04-03 17:25:11 +0800

    upadte

--- wt_themeeditor/window/windowsettings.h
+++ wt_themeeditor/window/windowsettings.h
@@ -1,4 +1,38 @@
 #ifndef WINDOWSETTINGS_H
 #define WINDOWSETTINGS_H
 
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QHBoxLayout>
+
+class WindowSettings:public QWidget{
+    Q_OBJECT
+public:
+    WindowSettings(QWidget *parent = 0);
+
+    void init();
+
+    QVBoxLayout *mLayoutSettings;
+
+    QWidget *srcDirDefault;
+    QWidget *srcDirOutput;
+    QWidget *srcDirCur;
+
+    QHBoxLayout *srcDefaultLayout;
+    QHBoxLayout *srcOutputLayout;
+    QHBoxLayout *srcCurLayout;
+
+    QLabel *srcDefaultLab;
+    QLabel *srcOutputLab;
+    QLabel *srcCurLab;
+
+    QLineEdit *srcDefaultEdit;
+    QLineEdit *srcOutputEdit;
+    QLineEdit *srcCurEdit;
+
+
+
+}
 #endif // WINDOWSETTINGS_H
